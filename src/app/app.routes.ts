edc84b61ae5f0b0d4bdaee790afb69fd0e5d7be5
Mayk29 <144456367+Mayk29@users.noreply.github.com>
2024-12-18 23:26:45 +0800

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { HomeComponent } from './admin/home/home.component';
+import { UserComponent } from './admin/user/user.component';
+import { PostComponent } from './admin/post/post.component';
+export const routes: Routes = [
+    {path:'user', component : UserComponent },
+    {path: 'post', component : PostComponent},
+    {path: '', component: HomeComponent }
+];
